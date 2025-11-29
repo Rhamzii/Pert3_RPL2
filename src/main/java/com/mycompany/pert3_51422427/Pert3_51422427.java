93010fdb5aebbc6a451aea20ef3547d70b0cb32d
Rhamzi <bwzramzi@gmail.com>
2025-11-29 09:38:00 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert3_51422427/Pert3_51422427.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_51422427;
+
+/**
+ *
+ * @author Farisz
+ */
+public class Pert3_51422427 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
